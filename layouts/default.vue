cfc16626de539020ecc4b5cc3626afb5f914a455
Marquis Abah <marquis.abah@shuttlers.ng>
2024-04-27 17:47:17 +0100

    added coach mobile number

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,8 +4,8 @@
     <NuxtPage />
     <FooterSection />
     <div class="fixed bottom-0 right-0 m-4 z-50">
-      <a href="https://wa.me/+447465803771" target="_blank">
-        <img src="@/assets/icons/chat.svg" class="text-green-500 h-16 w-16" />
+      <a href="https://wa.me/+2349019989878" target="_blank">
+        <img src="@/assets/icons/whatsapp.svg" class="text-green-500 h-16 w-16" />
       </a>
     </div>
   </div>
